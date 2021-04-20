d420b36892ede7a6e243f3399f4f87dc5d45b8b7
Lazare Fortune <30579785+lazarefortune@users.noreply.github.com>
2021-04-20 12:02:21 +0200

    correction du title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <meta name="description" content="">
       <meta name="author" content="">
-      <title>Gestion projets</title>
+      <title>Gestion des projets</title>
 
       <!-- Custom styles for this template -->
       <link href="css/sidebar.css" rel="stylesheet">
