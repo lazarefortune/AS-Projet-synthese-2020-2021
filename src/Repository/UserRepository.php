6959a42e08299bd58944f3ac3439c7567684d305
Lazare Fortune <lazarefortune@gmail.com>
2021-06-06 23:29:36 +0200

    Ajout du filtre des évaluateurs, dans liste des evaluateurs

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -36,6 +36,20 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->_em->flush();
     }
 
+    /**
+     * @return User[] Returns an array of User objects by role
+     */
+    public function findByRole(String $role)
+    {
+        $role = mb_strtoupper($role);
+        
+        return $this->createQueryBuilder('u')
+            ->andWhere('JSON_CONTAINS(u.roles, :role) = 1')
+            ->setParameter('role', '"ROLE_' . $role . '"')
+            ->getQuery()
+            ->getResult();
+    }
+    
     // /**
     //  * @return User[] Returns an array of Evaluateur objects
     //  */
